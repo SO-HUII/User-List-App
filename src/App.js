82162075117fb278be93580ec5861ef85c03c6de
SO HUI <haejuuun925@gmail.com>
2023-09-26 17:47:05 +0900

    feat: ErrorModal 컴포넌트 구현 및 Handler 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+    })
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler}/>
+      {/* UsersList에서 값을 추출하는 이름과 동일하게 설정해야 함 */}
+      {/* 배열로 설정해주지 않으면 UsersList에서 map undefined 에러 발생 */}
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     // form 제출 시 실행되는 함수
     const addUserHandler = (event) => {
@@ -14,13 +16,23 @@ const AddUser = (props) => {
 
         // 유효성 검사
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age (non-empty values).'
+            });
             return;
         }
         // +기호를 이용해 입력값을 숫자형으로 바꿀 수 있음
         if(+enteredAge < 1) {
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a valid age (> 0).'
+            });
             return;
         }
-        console.log(enteredUsername, enteredAge);
+
+        // Add User 버튼 클릭시 마다 입력된 데이터들을 App 컴포넌트로 보냄
+        props.onAddUser(enteredUsername, enteredAge);
 
         // 초기화
         setEnteredUsername('');
@@ -35,16 +47,23 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     };
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
         
     );
 };
