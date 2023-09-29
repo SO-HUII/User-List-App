f9037127ff817dd53041e5a42d710f41a4e62822
SO HUI <haejuuun925@gmail.com>
2023-09-29 20:43:15 +0900

    feat: Wrapper 컴포넌트 구현

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,7 @@ import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
-
+import Wrapper from "../Helpers/Wrapper";
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
@@ -52,7 +52,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -63,7 +63,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
         
     );
 };
