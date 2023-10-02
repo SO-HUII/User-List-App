85ddfd0a09dbe46217ae4c8b4e91c506088a94c3
SO HUI <haejuuun925@gmail.com>
2023-10-02 16:41:16 +0900

    feat: backdrop, modal 창 포탈로 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
@@ -12,12 +12,12 @@ function App() {
     })
   };
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler}/>
       {/* UsersList에서 값을 추출하는 이름과 동일하게 설정해야 함 */}
       {/* 배열로 설정해주지 않으면 UsersList에서 map undefined 에러 발생 */}
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
