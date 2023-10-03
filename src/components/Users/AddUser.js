e64a122f3d461c8548e34bae78652db3a52c513c
SO HUI <haejuuun925@gmail.com>
2023-10-03 15:53:00 +0900

    feat: state를 ref로 업데이트

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../Helpers/Wrapper";
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    // 사용자 이름을 입력할 수 있게 해주는 첫번째 input과 연결
+    // 생성되는 ref 값은 항상 '객체'
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [error, setError] = useState();
 
     // form 제출 시 실행되는 함수
@@ -14,8 +17,11 @@ const AddUser = (props) => {
         // submit 이벤트가 기본적으로 실행되도록 요청하는 정의된 동작을 중단하도록
         event.preventDefault();
 
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+
         // 유효성 검사
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non-empty values).'
@@ -23,7 +29,7 @@ const AddUser = (props) => {
             return;
         }
         // +기호를 이용해 입력값을 숫자형으로 바꿀 수 있음
-        if(+enteredAge < 1) {
+        if(+enteredUserAge < 1) {
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0).'
@@ -32,19 +38,11 @@ const AddUser = (props) => {
         }
 
         // Add User 버튼 클릭시 마다 입력된 데이터들을 App 컴포넌트로 보냄
-        props.onAddUser(enteredUsername, enteredAge);
+        props.onAddUser(enteredName, enteredUserAge);
 
-        // 초기화
-        setEnteredUsername('');
-        setEnteredAge('');
-    };
-
-    const usernameChangeHandler = event => {  // event 객체의 도움으로 입력된 값을 얻을 수 있음
-        setEnteredUsername(event.target.value);
-    };
-
-    const ageChangeHandler = event => {  // event 객체의 도움으로 입력된 값을 얻을 수 있음
-        setEnteredAge(event.target.value);
+        // input 초기화
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     };
 
     const errorHandler = () => {
@@ -57,9 +55,9 @@ const AddUser = (props) => {
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+                    <input id="username" type="text" ref={nameInputRef} />
                     <label htmlFor="age">Age (Years)</label>
-                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+                    <input id="age" type="number" ref={ageInputRef} />
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
